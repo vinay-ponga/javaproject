228b5531fe0f3da44647ff9f8f49e203d03bfc95
root <root@ip-172-31-23-103.ec2.internal>
2026-06-25 18:52:03 +0000

    First java project

---
+++ student.java
@@ -0,0 +1,10 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+
+        String name = "Rahul";
+        int age = 20;
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
